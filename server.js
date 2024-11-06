3576869f1c6fc36b420a492a8b7621300981dd8c
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-11-06 09:56:50 -0300

    Iniciando com Express

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de Espress API",
-    "/livros": "Entrei na rota livros",
-    "/autores": "Entrei na rota autores"
-};
-
-const server = http.createServer((req, res) =>{
-    res.writeHead(200, {"content-type": "text/plain"});
-    res.end(rotas[req.url]);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}/`);
 });
-
-server.listen(PORT, () => {
-    console.log("servidor escutando!");
-});
-
